3dc1512c47cb50047e61eb44e5c004d80bc75f16
Brandon Linton <linton.accounts@gmail.com>
2012-05-05 11:46:15 -0400

    Adding more int extensions for Hours, Minutes, Seconds, Milliseconds

---
+++ NetExtensions.Test/IntExtensionsTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using NetExtensions;
+
+namespace NetExtensions.Test
+{
+    public class IntExtensionsTest
+    {
+        [Fact]
+        public void CanDeriveDaysFromInt()
+        {
+            var result = 5.Days();
+
+            Assert.Equal(new TimeSpan(5, 0, 0, 0), result);
+            Assert.Equal(new TimeSpan(5, 0, 0, 0).Days, result.Days);
+        }
+
+        [Fact]
+        public void CanDeriveHoursFromInt()
+        {
+            var result = 5.Hours();
+
+            Assert.Equal(new TimeSpan(5, 0, 0), result);
+            Assert.Equal(new TimeSpan(5, 0, 0).Hours, result.Hours);
+        }
+
+        [Fact]
+        public void CanDeriveMinutesFromInt()
+        {
+            var result = 5.Minutes();
+
+            Assert.Equal(new TimeSpan(0, 5, 0), result);
+            Assert.Equal(new TimeSpan(0, 5, 0).Minutes, result.Minutes);
+        }
+
+        [Fact]
+        public void CanDeriveSecondsFromInt()
+        {
+            var result = 5.Seconds();
+
+            Assert.Equal(new TimeSpan(0, 0, 5), result);
+            Assert.Equal(new TimeSpan(0, 0, 5).Seconds, result.Seconds);
+        }
+
+        [Fact]
+        public void CanDeriveMillisecondsFromInt()
+        {
+            var result = 5.Milliseconds();
+
+            Assert.Equal(new TimeSpan(0, 0, 0, 0, 5), result);
+            Assert.Equal(new TimeSpan(0, 0, 0, 0, 5).Milliseconds, result.Milliseconds);
+        }
+    }
+}
---
+++ NetExtensions/IntExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetExtensions
+{
+    public static class IntExtensions
+    {
+        public static TimeSpan Days(this int number)
+        {
+            return new TimeSpan(days: number, hours: 0, minutes: 0, seconds: 0, milliseconds: 0);
+        }
+
+        public static TimeSpan Hours(this int number)
+        {
+            return new TimeSpan(hours: number, minutes: 0, seconds: 0);
+        }
+
+        public static TimeSpan Minutes(this int number)
+        {
+            return new TimeSpan(hours: 0, minutes: number, seconds: 0);
+        }
+
+        public static TimeSpan Seconds(this int number)
+        {
+            return new TimeSpan(hours: 0, minutes: 0, seconds: number);
+        }
+
+        public static TimeSpan Milliseconds(this int number)
+        {
+            return new TimeSpan(days: 0, hours: 0, minutes: 0, seconds: 0, milliseconds: number);
+        }
+    }
+}
