6c6e1e227af991be53a6db4a3b068a9c3a4c3a68
Brandon Linton <linton.accounts@gmail.com>
2012-05-04 23:54:40 -0400

    Second part of the ToCsv() extension commit (still learning git)

---
+++ NetExtensions.Test/StringExtensionsTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace NetExtensions.Test
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void CanCreateCsvStringFromStringEnumerable()
+        {
+            var strings = new [] { "a", "b", "c" };
+
+            var csv = strings.ToCsv();
+
+            Assert.Equal("a,b,c", csv);
+        }
+
+        [Fact]
+        public void StringEnumerableWithOneStringHasNoCommas()
+        {
+            var strings = new[] { "a" };
+
+            var csv = strings.ToCsv();
+
+            Assert.DoesNotContain(",", csv);
+            Assert.Equal("a", csv);
+        }
+
+        [Fact]
+        public void EmptyListReturnsStringEmpty()
+        {
+            var strings = new string[0];
+
+            var csv = strings.ToCsv();
+
+            Assert.Equal(string.Empty, csv);
+        }
+    }
+}
---
+++ NetExtensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetExtensions
+{
+    public static class StringExtensions
+    {
+        public static string ToCsv(this IEnumerable<string> strings)
+        {
+            return string.Join(",", strings.ToArray());
+        }
+    }
+}
