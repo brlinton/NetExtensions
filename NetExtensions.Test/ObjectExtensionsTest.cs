09cb982ad37d26350fc557015683c9def57b1a23
Brandon Linton <linton.accounts@gmail.com>
2012-05-14 21:22:56 -0400

    Adding Jon Skeet's beloved AsIf expression for objects

---
+++ NetExtensions.Test/ObjectExtensionsTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetExtensions;
+using Xunit;
+
+namespace NetExtensions.Test
+{
+    public class ObjectExtensionsTest
+    {
+        public interface ITester
+        {
+            int MethodCounter { get; set; }
+
+            void Method();
+        }
+
+        public class Tester : ITester
+        {
+            public int MethodCounter { get; set; }
+
+            public Tester()
+            {
+                MethodCounter = 0;
+            }
+
+            public void Method()
+            {
+                MethodCounter++;
+            }
+        }
+
+        [Fact]
+        public void AsIfOfCorrectTypeInvokesMethod()
+        {
+            var tester = new Tester();
+
+            tester.AsIf<ITester>(x => x.Method());
+
+            Assert.Equal(1, tester.MethodCounter);
+        }
+
+        [Fact]
+        public void AsIfOfIncorrectTypeInvokesNothing()
+        {
+            var tester = "not a tester object";
+
+            tester.AsIf<ITester>(x => x.Method());
+        }
+    }
+}
---
+++ NetExtensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace NetExtensions
+{
+    public static class ObjectExtensions
+    {
+        // http://stackoverflow.com/questions/7113347/c-sharp-assignment-in-an-if-statement
+        public static void AsIf<T>(this object obj, Action<T> action) where T : class
+        {
+            T t = obj as T;
+
+            if (t != null)
+                action(t);
+        }
+    }
+}
